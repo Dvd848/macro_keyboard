9dd032fbe5b55a2b043361625b4c2c1d39d96b0b
Dvd848 <Dvd848@users.noreply.github.com>
2021-06-19 14:36:58 +0300

    Add argument parsing

--- macro_keypad.py
+++ macro_keypad.py
@@ -2,9 +2,17 @@
 
 from input_device import linux_input, InputDevice
 from linux_input import struct_input_event, EventType, KeyEvent, Keys
+from typing import Callable, List, Dict
+
 import json
 import subprocess
 import os, pwd, grp
+import argparse
+import enum
+
+class Commands(enum.Enum):
+    LIST = "list"
+    RUN  = "run"
 
 
 # https://stackoverflow.com/questions/2699907/
@@ -27,40 +35,89 @@ def drop_privileges(uid_name = 'nobody', gid_name = 'nogroup'):
     # Ensure a very conservative umask
     os.umask(0o022)
 
-
-def handle_events(input_event: struct_input_event):
-
-    if input_event.type != EventType.EV_KEY.value:
-        return
-
-    if input_event.value != KeyEvent.KEY_UP.value:
-        return
-
-    key_code = linux_input.Keys(input_event.code)
-    print(f"Received key: {key_code}")
-
-    try:
-        print("-" * 6)
-        subprocess.run(action_mapping[key_code])
-        print(f"\n{'-' * 6}")
-    except KeyError:
-        pass
+def list_devices():
+    print("The following devices are connected:")
+    for device in os.listdir("/dev/input/by-id/"):
+        print(f" (-) {device}")
 
 
-action_mapping = {}
+def get_action_mapping(config_file: str) -> Dict[Keys, List[str]]:
+    action_mapping = {}
 
-with open("config.json") as f:
-    config = json.load(f)
-    for item in config["ActionMapping"]:
-        action_mapping[Keys[item["KeyCode"]]] = item["Action"]
+    with open(config_file) as f:
+        config = json.load(f)
+        for item in config["ActionMapping"]:
+            action_mapping[Keys[item["KeyCode"]]] = item["Action"]
 
-print(action_mapping)
+    return action_mapping
 
-with InputDevice("/dev/input/by-id/usb-04d9_1203-event-kbd") as device:
-    drop_privileges()
-    assert(os.getresuid() != (0, 0, 0))
+def print_keystrokes(device_path: str) -> None:
 
-    print(f"Connected to device '{device.name}'")
-    device.grab(True)
+    def handle_events(input_event: struct_input_event):
+        if input_event.type != EventType.EV_KEY.value:
+            return
 
-    device.loop_events(handle_events)
+        if input_event.value != KeyEvent.KEY_UP.value:
+            return
+
+        print(f"\nReceived keystroke: {Keys(input_event.code)}")
+
+    run(device_path, False, handle_events)
+
+def run_macro_keypad(device_path: str, action_mapping: Dict[Keys, List[str]]) -> None:
+
+    def handle_events(input_event: struct_input_event):
+        if input_event.type != EventType.EV_KEY.value:
+            return
+
+        if input_event.value != KeyEvent.KEY_UP.value:
+            return
+
+        key_code = linux_input.Keys(input_event.code)
+
+        if key_code in action_mapping:
+            print("Running command:\n{}".format(action_mapping[key_code]))
+            subprocess.run(action_mapping[key_code])
+            print("\nDone")
+            print("-" * 20)
+        
+
+    run(device_path, True, handle_events)
+
+def run(device_path: str, grab_device: bool, handler: Callable[[struct_input_event], None]) -> None:
+    with InputDevice(device_path) as device:
+        drop_privileges()
+        assert(os.getresuid() != (0, 0, 0))
+
+        print(f"Connected to device '{device.name}'")
+        if grab_device:
+            device.grab(True)
+
+        device.loop_events(handler)
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='A program to utilize a dedicated keyboard as a macro keyboard')
+
+    subparsers = parser.add_subparsers(dest='command', required = True, title='subcommands',
+                                       description='Valid subcommands')
+    # A list command
+    list_parser = subparsers.add_parser(Commands.LIST.value, help='List the devices under /dev/input/by-id/')
+
+    # A run command
+    run_parser = subparsers.add_parser(Commands.RUN.value, help='Attach to keyboard device and handle keystrokes')
+    run_parser.add_argument('-d', '--device', action='store', help="The device path to connect to", required = True)
+    run_action = run_parser.add_mutually_exclusive_group(required = True)
+    run_action.add_argument('-p', '--print-keystrokes', action='store_true', help="Interactively print the user keystrokes")
+    run_action.add_argument('-m', '--macro', action='store', type=str, metavar=('CONFIG_FILE'), help="Execute macros with the given configuration file")
+
+    args = parser.parse_args()
+
+    if args.command == Commands.LIST.value:
+        list_devices()
+    elif args.command == Commands.RUN.value:
+        if args.print_keystrokes:
+            print_keystrokes(args.device)
+        elif args.macro:
+            run_macro_keypad(args.device, get_action_mapping(args.macro))
